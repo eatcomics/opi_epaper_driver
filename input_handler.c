aaaf6a37108ca8a710a93e5ada8e7c261618e8a5
irpo <irpo@irpo.me>
2026-03-15 22:11:09 -0500

    does this work?

--- input_handler.c
+++ input_handler.c
@@ -127,7 +127,7 @@ int read_key_event(uint32_t *keycode, int *modifiers) {
 
 size_t check_keys(uint8_t *buf) {
     // this isn't initialized and I don't know what it's supposed to be... ruh roh
-    uint32_t keycode;
+    uint32_t keycode = NULL; // does making it NULL work?
 
     if (input->len >= MAX_KEY_BUFFER) {
         input->len = 0;
