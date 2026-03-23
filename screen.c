5c1aa38a12ca98d0e8ea9ec06d198f5270bdabff
irpo <irpo@irpo.me>
2026-03-22 20:17:24 -0500

    i dunno

--- screen.c
+++ screen.c
@@ -194,12 +194,12 @@ static void screen_render() {
 void map_doc_coords(uint8_t *doc, size_t doc_len, size_t cursor) {
     int row, col = 0;
     
-    for (size_t i = 0; i < doc_len; i++) {
+    for (int i = 0; i < doc_len; i++) {
         if (i < 0) {
             printf("i is > 0 - Breaking\n");
             break;
         } else if (i > 1920) {
-            printf("i is < 1920 - Breaking]n");
+            printf("i is < 1920 - Breaking\n");
             break;
         } else {
             printf("is i mod 80 == 0? ");
